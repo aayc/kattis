9b68113c9808c04e3b19554f0d88f8354d277e77
Aaron Chan <aaron.y.chan64@gmail.com>
2019-09-27 09:43:50 -0600

    tried 10 kinds of people and it works but is too slow (need to use union find)

---
+++ 10kindsofpeople/10kindsofpeople.py
@@ -0,0 +1,33 @@
+from collections import deque
+# Ugh too slow, need to use union find disjoint
+
+def solution(a, b, y, z):
+    q = deque([(a, b)])
+    V = set()
+    while q:
+        r, c = q.popleft()
+        if r == y and c == z:
+            return True
+
+        neighbors = [(r - 1, c), (r, c - 1), (r + 1, c), (r, c + 1)]
+        for neighbor in neighbors:
+            nr, nc = neighbor
+            if 0 <= nr < len(M) and 0 <= nc < len(M[0]) and (nr, nc) not in V and M[nr][nc] == M[r][c]:
+                V.add((nr, nc))
+                q.append((nr, nc))
+
+    return False
+
+R, C = [int(x) for x in input().split(" ")]
+M = []
+for i in range(R):
+    M.append(list(input()))
+
+T = int(input())
+for t in range(T):
+    a, b, c, d = [int(x) for x in input().split(" ")]
+    user = "decimal" if M[a - 1][b - 1] == '1' else "binary"
+    if solution(a - 1, b - 1, c - 1, d - 1):
+        print(user)
+    else:
+        print("neither")
